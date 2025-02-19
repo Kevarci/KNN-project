b912e089d904a65fcea598bcd6181a68734615aa
Kevin arciniegas <stickmarq@gmail.com>
2025-02-19 22:24:20 +0000

    primeros cambios en el proyecto

--- src/app.py
+++ src/app.py
@@ -1,4 +1,43 @@
-from utils import db_connect
-engine = db_connect()
+import pandas as pd 
+from sqlalchemy import create_engine
 
+url1 = 'https://raw.githubusercontent.com/4GeeksAcademy/k-nearest-neighbors-project-tutorial/main/tmdb_5000_movies.csv'
+
+url2 = 'https://raw.githubusercontent.com/4GeeksAcademy/k-nearest-neighbors-project-tutorial/main/tmdb_5000_credits.csv'
 # your code here
+
+movie_df = pd.read_csv(url1)
+credits_df = pd.read_csv(url2)
+
+#print(movie_df)
+#print(credits_df)
+
+
+#configuro la conexion de la Bdd
+db_url = 'postgresql://gitpod@localhost:5432/movie_db'
+eng = create_engine(db_url)
+
+movie_df.to_sql('movies', eng, if_exists='replace', index=False)
+credits_df.to_sql('credits', eng, if_exists='replace', index=False)
+
+#Conexion a la Bdd 
+with eng.connect() as connection:
+    query = """
+        CREATE TABLE unifica_movies AS
+        SELECT 
+            m.id AS movie_id,
+            m.title,
+            m.overview,
+            m.genres,
+            m.keywords,
+            c.cast,
+            c.crew
+        FROM movies m 
+        JOIN credicts c 
+        ON m.title = c.title;
+        """
+    connection.execute(query)
+
+dfUnificada = pd.read_sql_table('unified_movies', eng)
+
+print(dfUnificada.columns)
